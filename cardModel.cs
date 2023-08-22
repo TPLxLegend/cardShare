86e64b30f8ccd0b4410710afc06882b6bf279719
cygnusDownfall <phapsuhacam112@gmail.com>
2023-08-22 19:40:09 +0700

    quicksave

---
+++ cardModel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class cardModel : ScriptableObject
+{
+    #region properties
+    public string CardName;
+    public string CardType;
+
+    public string CardDescription;
+    public Sprite icon;
+    public int Count;
+
+    #endregion
+
+    #region methods
+    
+    #endregion
+}
