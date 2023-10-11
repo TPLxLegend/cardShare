507692af98c0d6a78c073db33ebe648f4e393527
cygnusDownfall <phapsuhacam112@gmail.com>
2023-10-11 20:57:00 +0700

    cardModel to card repo

---
+++ ClassModel/constant.cs
@@ -0,0 +1,57 @@
+public enum DmgType{
+    Physic,
+    Fire,
+    Ice,
+    Electric,
+    Poison,
+    Dark,
+    Light,
+}
+public enum typeTarget
+{
+    seft,
+    enemys,
+    allys
+}
+public enum charJob
+{
+    all,
+    warrior,
+    magician,
+    thief,
+    assasin,
+    knight,
+    paladin,
+    wizzard,
+    swordman,
+    archer
+}
+public enum cardType
+{
+    attack,
+    trap,
+    spell,
+}
+public enum cardTag{
+    attack,
+    buff,
+    neft,
+    curse,
+    blessing,
+    elemental
+}
+public enum targetFilterType{
+    allObj,
+    player,
+    enemy,
+    ally,
+    seft
+}
+public enum triggerType{
+    whenHitSomething,
+    whenHitPlayer,
+    whenHitEnemy,
+    whenHitAlly,
+    whenHitSeft,
+    inArea
+}
